22886bd1dfb810f8b6099a33dfb521b31a7c2685
joaoluis25 <180711519+joaoluis25@users.noreply.github.com>
2025-12-10 19:25:22 -0300

    imgs slide

---
+++ projetos 2 periodo/slide [2]/slide.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const images = [
+    {'id': '1', 'url': './imgs/img1.png'},
+    {'id': '2', 'url': './imgs/img2.png'},
+    {'id': '3', 'url': './imgs/img3.png'},
+    {'id': '4', 'url': './imgs/img4.png'},
+    {'id': '5', 'url': './imgs/img5.png'}
+]
+
+const container = document.querySelector('#container-items')
+
+const loadImages = (images, container) =>{
+    images.forEach(image => {
+        
+    });
+}
+
+loadImages(images, container)
