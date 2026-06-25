6cbf9bed3ca56db90123807054325f3be36fbf10
joaoluis25 <180711519+joaoluis25@users.noreply.github.com>
2026-06-24 23:53:14 -0300

    moving clouds js finish

---
+++ projetos 3 periodo/moving-clouds/index.js
@@ -0,0 +1,13 @@
+const elements = document.getElementsByClassName("someName");
+const element = elements[0];
+element.style.background = "red";
+const elements = document.getElementsByClassName('cloud');
+const element = elements[0];
+element.style.top = '80px';
+element.style.left = '120px';
+
+(Math.random() * 100) + '%'
+element.style.left = (Math.random() * 100) + '%'
+
+element.style.transform = "scale(0.3)"
+(Math.random() * 0.7) + 0.3
