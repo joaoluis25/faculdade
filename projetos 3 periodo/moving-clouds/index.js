31c3169c588d5a22202f84251e95e4c88434d6e9
joaoluis25 <180711519+joaoluis25@users.noreply.github.com>
2026-06-26 23:41:23 -0300

    clouds project

--- projetos 3 periodo/moving-clouds/index.js
+++ projetos 3 periodo/moving-clouds/index.js
@@ -1,13 +1,36 @@
-const elements = document.getElementsByClassName("someName");
-const element = elements[0];
-element.style.background = "red";
+
+const containerElement = document.getElementById('container')
+containerElement.innerHTML = `
+<div class="cloud">
+    <div class="cloudBubble1"></div>
+    <div class="cloudBubble2"></div>
+</div>`.repeat(20)
+
 const elements = document.getElementsByClassName('cloud');
-const element = elements[0];
-element.style.top = '80px';
-element.style.left = '120px';
+for (let j = 0; j < elements.length; j++) {
+    const element = elements[j];
 
-(Math.random() * 100) + '%'
-element.style.left = (Math.random() * 100) + '%'
+    element.style.top = (Math.random() * 100) + '%'
+    element.style.left = (Math.random() * 100) + '%'
+    element.style.transform = "scale(" + ((Math.random() * 0.7) + 0.3) + ")"
 
-element.style.transform = "scale(0.3)"
-(Math.random() * 0.7) + 0.3
+    const intervalTimeInSeconds = (Math.random() * 20) + 6
+    element.style.transition = "all linear " + intervalTimeInSeconds + "s"
+
+    const startRight = Math.random() > 0.5
+
+    setTimeout(() => {
+        element.style.left = startRight ? '100%' : '-50%'
+    }, [1000])
+
+    i = startRight ? 1 : 0
+    setInterval(() => {
+        if (i % 2 === 0) {
+            element.style.left = '100%'
+        } else {
+            element.style.left = '-50%'
+        }
+        i += 1
+    }, intervalTimeInSeconds * 1000)
+}
+
