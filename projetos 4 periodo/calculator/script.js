fdc9621a8ab9c891c3a40b5d37c73e800f7b4948
joaoluis25 <180711519+joaoluis25@users.noreply.github.com>
2026-07-05 21:32:58 -0300

    project calculate complete

---
+++ projetos 4 periodo/calculator/script.js
@@ -0,0 +1,17 @@
+let display = document.getElementById("display");
+
+function appendToDisplay(value) {
+    display.value += value;
+}
+
+function clearDisplay() {
+    display.value = "";
+}
+
+function deleteLast() {
+    display.value = display.value.slice(0, -1);
+}
+
+function calculateResult() {
+    display.value = eval(display.value);
+}
